b4ef48e759230fe96eb70a828c69bec40875873e
Arif Acar <arifacar@gmail.com>
2023-01-16 05:19:37 +0300

    add document for "Circuit Breaker Pattern"

--- resilience4j/README.md
+++ resilience4j/README.md
@@ -9,3 +9,69 @@ This module contains the implementation of Resilience4j.
 - Wiremock
 - Awaitility
 - JUnit
+
+# Circuit Breaker Pattern
+
+# **Circuit Breaker**
+
+`minimum-number-of-calls=5`
+
+Sigortanın açık konuma geçmesini veya kapalı konumda kalmasını sağlayacak hatalı/yavaş çağrı oranlarının hesaplanması için yapılması gereken asgari çağrı sayısını belirtir.
+
+`permitted-number-of-calls-in-half-open-state=3`
+
+Sigorta yarı-açık konumda iken kritik bölgeye yapılabilecek çağrı sayısıdır.
+
+`automatic-transition-from-open-to-half-open-enabled=true`
+
+Sigortanın açık konumdan yarı-açık konuma el yordamı gerekmeksizin geçebilmesini sağlar (`true` ise). Bu özelliğin `false` değerine sahip olması durumunda sigorta, ancak el yordamıyla yarı-açık konuma geçirilebilir.
+
+`wait-duration-in-open-state=5s`
+
+Sigortanın açık konumdan yarı-açık konuma geçmesi için geçmesi beklenen süredir
+
+# Retry
+
+**@Retry(name =”`retryApi`”)** - N defa tekrar denemesini sağlar.
+
+`resilience4j.retry.instances.**retryApi**.max-attempts=3`
+
+3 defa denedikten sonra hard coded olarak döneceği mesaj.
+
+`@Retry(name = "retryApi", fallbackMethod = "**fallbackAfterRetry**")`
+
+```java
+public String **fallbackAfterRetry**(Exception ex) {
+        return "all retries have exhausted";
+    }
+```
+
+`resilience4j.retry.instances.retryApi.wait-duration=1s`
+
+1 sn aralıkla deneme yapsın. 3x1 = 3 sn ekstra deneme olacak.
+
+`resilience4j.retry.instances.retryApi.exponential-backoff-multiplier=2`
+
+önce 1 sn sonra 2 sonra da 4 sn aralıkla dener.
+
+# Rate Limiter
+
+Örneğin 10 sn içinde `rateLimiterApi` ’e saniyede 10bin isteğe izin veriyorum demek istediğimizde kullanırız.
+
+`@RateLimiter(name = "rateLimiterApi")`
+
+`resilience4j.ratelimiter.instances.rateLimiterApi.limit-for-period=100`
+
+`resilience4j.ratelimiter.instances.rateLimiterApi.limit-refresh-period=60s`
+
+Her 60 saniyede 100 isteğe izin ver gibi.
+
+# ****BulkHead****
+
+Aynı anda kaç tane isteğe yanıt verebileceğini bildiririz.
+
+`resilience4j.bulkhead.instances.bulkheadApi.max-concurrent-calls=3`
+
+`resilience4j.bulkhead.instances.bulkheadApi.max-wait-duration=1`
+
+En fazla 3 eşzamanlı çağrıda bulunabilir.
--- resilience4j/src/test/java/com/arifacar/resilience4j/ResilientAppControllerManualTest.java
+++ resilience4j/src/test/java/com/arifacar/resilience4j/ResilientAppControllerManualTest.java
@@ -4,8 +4,10 @@ import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,6 +26,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.http.HttpStatus.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ResilientAppControllerManualTest {
 
     @RegisterExtension
@@ -37,7 +40,9 @@ class ResilientAppControllerManualTest {
     private TestRestTemplate restTemplate;
 
     @Test
+    @Order(1)
     public void testCircuitBreaker() {
+        System.out.println("::: START CB :::");
         EXTERNAL_SERVICE.stubFor(WireMock.get("/api/external")
             .willReturn(serverError()));
 
@@ -45,19 +50,24 @@ class ResilientAppControllerManualTest {
             .forEach(i -> {
                 ResponseEntity<String> response = restTemplate.getForEntity("/api/circuit-breaker", String.class);
                 assertThat(response.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+                System.out.println(response.getStatusCode());
             });
 
         IntStream.rangeClosed(1, 5)
             .forEach(i -> {
                 ResponseEntity<String> response = restTemplate.getForEntity("/api/circuit-breaker", String.class);
                 assertThat(response.getStatusCode()).isEqualTo(HttpStatus.SERVICE_UNAVAILABLE);
+                System.out.println(response.getStatusCode());
             });
 
         EXTERNAL_SERVICE.verify(5, getRequestedFor(urlEqualTo("/api/external")));
+        System.out.println("::: END CB :::");
     }
 
     @Test
+    @Order(2)
     public void testRetry() {
+        System.out.println("::: START RETRY :::");
         EXTERNAL_SERVICE.stubFor(WireMock.get("/api/external")
             .willReturn(ok()));
         ResponseEntity<String> response1 = restTemplate.getForEntity("/api/retry", String.class);
@@ -70,10 +80,13 @@ class ResilientAppControllerManualTest {
         ResponseEntity<String> response2 = restTemplate.getForEntity("/api/retry", String.class);
         Assert.assertEquals(response2.getBody(), "all retries have exhausted");
         EXTERNAL_SERVICE.verify(3, getRequestedFor(urlEqualTo("/api/external")));
+        System.out.println("::: END RETRY :::");
     }
 
     @Test
+    @Order(3)
     public void testBulkhead() {
+        System.out.println("::: START BH :::");
         EXTERNAL_SERVICE.stubFor(WireMock.get("/api/external")
             .willReturn(ok()));
         Map<Integer, Integer> responseStatusCount = new ConcurrentHashMap<>();
@@ -91,36 +104,45 @@ class ResilientAppControllerManualTest {
         assertTrue(responseStatusCount.containsKey(BANDWIDTH_LIMIT_EXCEEDED.value()));
         assertTrue(responseStatusCount.containsKey(OK.value()));
         EXTERNAL_SERVICE.verify(3, getRequestedFor(urlEqualTo("/api/external")));
+        System.out.println("::: END BH :::");
     }
 
     @Test
+    @Order(4)
     public void testTimeLimiter() {
+        System.out.println("::: START TL :::");
         EXTERNAL_SERVICE.stubFor(WireMock.get("/api/external")
             .willReturn(ok()));
         ResponseEntity<String> response = restTemplate.getForEntity("/api/time-limiter", String.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.REQUEST_TIMEOUT);
         EXTERNAL_SERVICE.verify(1, getRequestedFor(urlEqualTo("/api/external")));
+        System.out.println("::: END TL :::");
     }
 
     @Test
+    @Order(5)
     public void testRatelimiter() {
+        System.out.println("::: START RL :::");
         EXTERNAL_SERVICE.stubFor(WireMock.get("/api/external")
             .willReturn(ok()));
         Map<Integer, Integer> responseStatusCount = new ConcurrentHashMap<>();
 
-        IntStream.rangeClosed(1, 50)
+        IntStream.rangeClosed(1, 500)
             .parallel()
             .forEach(i -> {
                 ResponseEntity<String> response = restTemplate.getForEntity("/api/rate-limiter", String.class);
                 int statusCode = response.getStatusCodeValue();
-                responseStatusCount.put(statusCode, responseStatusCount.getOrDefault(statusCode, 0) + 1);
+                int value = responseStatusCount.getOrDefault(statusCode, 0) + 1;
+                responseStatusCount.put(statusCode, value);
+                System.out.println(i + " - ST: " + statusCode + " - C: " + value);
             });
 
         assertEquals(2, responseStatusCount.keySet()
             .size());
         assertTrue(responseStatusCount.containsKey(TOO_MANY_REQUESTS.value()));
         assertTrue(responseStatusCount.containsKey(OK.value()));
-        EXTERNAL_SERVICE.verify(5, getRequestedFor(urlEqualTo("/api/external")));
+        EXTERNAL_SERVICE.verify(100, getRequestedFor(urlEqualTo("/api/external")));
+        System.out.println("::: END RL :::");
     }
 }
