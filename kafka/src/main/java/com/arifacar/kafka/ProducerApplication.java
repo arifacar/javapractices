6c7be65eb2da6bb4fdefdbc6425d4b6c467b0bdb
Arif Acar <arifacar@gmail.com>
2023-01-19 05:13:05 +0300

    create first producer and consumer app

--- kafka/src/main/java/com/arifacar/kafka/ProducerApplication.java
+++ kafka/src/main/java/com/arifacar/kafka/ProducerApplication.java
@@ -1,33 +1,34 @@
 package com.arifacar.kafka;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
 
-/**
- * Consumer App
- */
 @SpringBootApplication
-public class KafkaApplication {
+public class ProducerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KafkaApplication.class, args);
+        SpringApplication.run(ProducerApplication.class, args);
     }
 
-    //@Bean
+    @Bean
     public NewTopic topic() {
         return TopicBuilder.name("topicPerson")
                 .partitions(5)
-                .replicas(2)
+                .replicas(1)
                 .build();
     }
 
-    @KafkaListener(id = "test", topics = "topicPerson")
-    public void listen(String in) {
-        System.out.println(in);
+    @Bean
+    public ApplicationRunner runner(KafkaTemplate<String, String> template) {
+        return args -> {
+            template.send("topicPerson", "Arif Acar");
+            template.send("topicPerson", "Turgut Acar");
+        };
     }
 
 }
