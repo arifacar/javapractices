fd6bcbf0dea890046b081fcaca4341f01e645b85
Arif Acar <arifacar@gmail.com>
2023-01-20 05:58:27 +0300

    java util queue test

---
+++ data-structure/src/main/java/com/arifacar/datastructure/queue/JavaUtilQueueTest.java
@@ -0,0 +1,33 @@
+package com.arifacar.datastructure.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+public class JavaUtilQueueTest {
+
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(6);
+        queue.add(5);
+        queue.add(4);
+        queue.add(3);
+        queue.add(2); // offer da ekleme yapar hata fırlatmaz
+
+        System.out.println("QUEUE LIST: ");
+        queue.stream().forEach(System.out::println);
+
+        System.out.println("REMOVING: ");
+        System.out.println(queue.remove()); // 6  - remove ve poll aynı, poll hata fırlatmaz.
+        System.out.println(queue.remove()); // 5
+        System.out.println(queue.remove()); // 4
+
+        System.out.println("FIRST ITEM: ");
+        System.out.println(queue.element()); // element ve peek,  peek hata fırlatmaz.
+
+        System.out.println("ITEM SIZE: ");
+        System.out.println(queue.size());
+
+        System.out.println("LAST STATUS: ");
+        queue.stream().forEach(System.out::println);
+    }
+
+}
